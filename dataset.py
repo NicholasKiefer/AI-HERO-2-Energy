b42a11d8f8d1d097cf969c43a5afc0d7593a1251
David Li <david.li@kit.edu>
2023-06-20 14:22:45 +0200

    change submit script for first test

--- dataset.py
+++ dataset.py
@@ -14,18 +14,13 @@ from typing import Optional
 
 
 class DroneImages(torch.utils.data.Dataset):
-<<<<<<< HEAD
     def __init__(self, root: str = 'data', max_images: Optional[int] = None, downsample_ratio:Optional[int] = None, augment:Optional[bool] = False):
-=======
-    def __init__(self, root: str = 'data', downsample_ratio: Optional[int] = None):
->>>>>>> main
         self.root = pathlib.Path(root)
         self.parse_json(self.root / 'descriptor.json')
         self.downsample_ratio = downsample_ratio
         self.augment = augment
         
         if downsample_ratio is not None:
-<<<<<<< HEAD
             os.makedirs(self.root + f"_{downsample_ratio}", exist_ok=True)
             sampled_path = self.root + f"_{downsample_ratio}"
             self.sampled = {key: sampled_path + f"/{self.images[key]}" for key in self.ids if os.path.exists(sampled_path + f"/{self.images[key]}")}
@@ -39,13 +34,6 @@ class DroneImages(torch.utils.data.Dataset):
             self.augmentation = ab.Compose([ab.RandomCrop(480, 640), ], ab.BboxParams("pascal_voc"))  # these need equivalent for bounding box/seg mask
 
     def parse_json(self, path: pathlib.Path, max_images: Optional[int] = None):
-=======
-            sampled_path = self.root.parent / (self.root.name + f"_{downsample_ratio}")
-            os.makedirs(sampled_path, exist_ok=True)
-            self.sampled = {key: sampled_path / f"{self.images[key]}" for key in self.ids if os.path.exists(sampled_path / f"{self.images[key]}")}
-
-    def parse_json(self, path: pathlib.Path):
->>>>>>> main
         """
         Reads and indexes the descriptor.json
 
@@ -120,13 +108,7 @@ class DroneImages(torch.utils.data.Dataset):
             'labels': labels,  # Int64Tensor[N]
             'masks': masks,  # UIntTensor[N, H, W]
         }
-<<<<<<< HEAD
         x = self.transforms(x)
-=======
-        x = torch.tensor(x, dtype=torch.float).permute((2, 0, 1))
-        if self.downsample_ratio is not None:
-            x = torch.nn.functional.max_pool2d(x, kernel_size=(self.downsample_ratio, self.downsample_ratio))
->>>>>>> main
         x = x / 255.
         
         if self.augment:
--- train.sh
+++ train.sh
@@ -3,9 +3,9 @@
 #SBATCH --job-name=AI-HERO_energy_baseline_training
 #SBATCH --partition=accelerated
 #SBATCH --nodes=1
-#SBATCH --gres=gpu:1
-#SBATCH --ntasks=1
-#SBATCH --ntasks-per-node=1
+#SBATCH --gres=gpu:4
+#SBATCH --ntasks=4
+#SBATCH --ntasks-per-node=4
 #SBATCH --cpus-per-task=76
 #SBATCH --time=20:00:00
 
@@ -13,12 +13,12 @@ export CUDA_CACHE_DISABLE=1
 export OMP_NUM_THREADS=76
 
 data_workspace=/hkfs/work/workspace/scratch/ih5525-E3/datasets/
-group_workspace=/hkfs/work/workspace/scratch/ih5525-E3/AI-HERO-2-Energy
+group_workspace=/hkfs/work/workspace/scratch/ih5525-E3
 
 module load compiler/gnu/11
 module load mpi/openmpi/4.0
 module load lib/hdf5/1.12
 module load devel/cuda/11.8
 
-source ${group_workspace}/energy_baseline_env/bin/activate
-srun python ${group_workspace}/train.py --batch 16 --epochs 10 --lr 1e-3 --root ${data_workspace}
+source ${group_workspace}/energy_venv/bin/activate
+srun python ${group_workspace}/AI-HERO-2-Energy/train.py --batch 4 --epochs 2 --lr 1e-3 --root ${data_workspace}
