bd71737d862ea3df67af234135d76f087ae7a98d
David Li <david.li@kit.edu>
2023-06-20 14:05:54 +0200

    Revert "changed submit script for first test run"
    This reverts commit 8e6fd66e1a834ed411d95bce7277648ec90e5cd0.

--- train.sh
+++ train.sh
@@ -3,9 +3,9 @@
 #SBATCH --job-name=AI-HERO_energy_baseline_training
 #SBATCH --partition=accelerated
 #SBATCH --nodes=1
-#SBATCH --gres=gpu:4
-#SBATCH --ntasks=4
-#SBATCH --ntasks-per-node=4
+#SBATCH --gres=gpu:1
+#SBATCH --ntasks=1
+#SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=76
 #SBATCH --time=20:00:00
 
@@ -13,12 +13,12 @@ export CUDA_CACHE_DISABLE=1
 export OMP_NUM_THREADS=76
 
 data_workspace=/hkfs/work/workspace/scratch/ih5525-E3/datasets/
-group_workspace=/hkfs/work/workspace/scratch/ih5525-E3/AI-HERO-2-Energy/ih5525-E3/
+group_workspace=/hkfs/work/workspace/scratch/ih5525-E3/AI-HERO-2-Energy
 
 module load compiler/gnu/11
 module load mpi/openmpi/4.0
 module load lib/hdf5/1.12
 module load devel/cuda/11.8
 
-source ${group_workspace}/energy_venv/bin/activate
+source ${group_workspace}/energy_baseline_env/bin/activate
 srun python ${group_workspace}/train.py --batch 16 --epochs 10 --lr 1e-3 --root ${data_workspace}
