89756cc5f8b0129e89e642196deae81eb4b2c903
David Li <david.li@kit.edu>
2023-06-20 17:00:40 +0200

    fixed dictionary error in transpose loop

--- train.py
+++ train.py
@@ -131,7 +131,7 @@ def train(hyperparameters: argparse.Namespace):
                 model.eval()
                 train_predictions = model(x)
                 for i,item in enumerate(train_predictions):
-                    train_predictions[i] = torch.transpose(item,-1,-2)
+                    train_predictions[i]['masks'] = torch.transpose(item['masks'],-1,-2)
                 train_metric(*to_mask(train_predictions, label))
                 model.train()
             writer.add_scalar("loss/train", loss.item(), epoch)
@@ -163,7 +163,7 @@ def train(hyperparameters: argparse.Namespace):
             with torch.no_grad():
                 test_predictions = model(x_test)
                 for i,item in enumerate(test_predictions):
-                    test_predictions[i] = torch.transpose(item,-1,-2)
+                    test_predictions[i]['masks'] = torch.transpose(item['masks'],-1,-2)
                 test_metric(*to_mask(test_predictions, test_label))
 
         writer.add_scalar("iou/test", test_metric.compute())
